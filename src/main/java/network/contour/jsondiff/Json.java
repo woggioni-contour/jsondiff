fb150a55acade5a5f7805ea3874db83b74ab9a8b
woggioni-contour <97496201+woggioni-contour@users.noreply.github.com>
2022-07-08 13:25:20 +0800

    Initial commit

---
+++ src/main/java/network/contour/jsondiff/Json.java
@@ -0,0 +1,26 @@
+package network.contour.jsondiff;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.SneakyThrows;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Json {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @SneakyThrows
+    public static JsonNode parse(Path path)  {
+        try(Reader reader = Files.newBufferedReader(path)) {
+            return objectMapper.readTree(reader);
+        }
+    }
+
+    public static ObjectWriter objectWriter(boolean prettyPrint) {
+        return prettyPrint ? objectMapper.writerWithDefaultPrettyPrinter() : objectMapper.writer();
+    }
+}
---
+++ src/main/java9/module-info.java
@@ -0,0 +1,6 @@
+open module network.contour.jsondiff {
+    requires static lombok;
+    requires info.picocli;
+    requires com.fasterxml.jackson.databind;
+    requires com.github.fge.jsonpatch;
+}

f530baf2835c86fd443cfdc920f7251907461565
Walter Oggioni <walter.oggioni@contour.network>
2022-08-24 11:26:47 +0800

    switch to version catalog

--- src/test/java/network/contour/jsondiff/JsonDiffTest.java
+++ src/test/java/network/contour/jsondiff/JsonDiffTest.java
@@ -7,36 +7,34 @@ import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.diff.JsonDiff;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.net.URL;
+import java.util.Objects;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JsonDiffTest {
-
-    @RequiredArgsConstructor
-    enum TestCase {
-        A("[1, 2, 3, 4]", "[1, 3, 4]", ""),
-        B("{\"a\" : [1,2,3]}", "{\"a\": [1,3], \"b\" : [false, null, \"hello\"]}", ""),
-        C("{\"a\" : {\"3/4\": [1,2,3]}}", "{\"a\": {\"3/4\": [1,3]}}", ""),
-        D("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", "[1, 3, 4, 5, 6, 7, 8, 9, 10, 2]", "");
-
-        private final String json1;
-        private final String json2;
-        private final String expectedDiff;
-    }
-
-
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @SneakyThrows
     @ParameterizedTest
-    @EnumSource(TestCase.class)
-    void test(TestCase testCase) {
-        JsonNode node1 = objectMapper.readTree(testCase.json1);
-        JsonNode node2 = objectMapper.readTree(testCase.json2);
+    @ValueSource(strings = {"A", "B", "C", "D"})
+    void test(String testCase) {
+        URL resource = getClass().getResource("test/" + testCase + ".json");
+        Objects.requireNonNull(resource);
+        JsonNode testCaseDocument = objectMapper.readTree(resource.openStream());
+        JsonNode node1 = testCaseDocument.get("doc1");
+        JsonNode node2 = testCaseDocument.get("doc2");
         JsonPatch patch = JsonDiff.asJsonPatch(node1, node2);
         JsonNode patchNode = objectMapper.valueToTree(patch);
         ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
         writer.writeValue(System.out, patchNode);
         System.out.println();
+        JsonNode expectedDiff = testCaseDocument.get("diff");
+        Assertions.assertEquals(expectedDiff, patchNode);
     }
 }
